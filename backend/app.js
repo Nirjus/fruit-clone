73bacbd07324997843555d2ce247e1fd8c461ba3
Nirjus Karmakar <karmakarnirjus4839@gmail.com>
2025-01-03 19:21:36 +0530

    git initialize

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 import express from "express";
 import productRouter from "./routes/products.route.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use("/test", (req, res) => {
   res.send("Hello Backend is running 👌");
@@ -14,6 +20,7 @@ app.use("/product", productRouter);
 
 app.use((error, req, res, next) => {
   console.error(err.stack);
+
   res.status(500).send("Something broke!");
 });
 export default app;
